27c2d5f933bd8965327ae44c0968701468626d10
Dima <dimader@bk.ru>
2021-12-17 19:43:07 +0700

    Демонстрация работы с двумерным массивом объектов класса

--- Test.java
+++ Test.java
@@ -1,26 +1,21 @@
 public class Test {
 
    public static void main(String args[]){
-   		// Создание объекта конструктором без параметров
-		bus my_bus = new bus();
-		my_bus.add_passenger();
-		try
-		{
-			int check = my_bus.remove_passenger();
-			if (check == -1) throw new Exception("\nАвтобус пуст.\n");
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.getMessage());
-		}
-		try
-		{
-			int check = my_bus.remove_passenger();
-			if (check == -1) throw new Exception("\nАвтобус пуст.\n");
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.getMessage());
+   		bus[][] bus_array = new bus[2][2];
+   		int bus_num = 1;
+   		for (int i = 0; i < 2; i++) {
+   			for (int j = 0; j < 2; j++) {
+   				bus_array[i][j] = new bus(bus_num);
+   				bus_num++;
+   			}
+   		}
+   		bus_array[0][0].set_price(25);
+		bus_array[0][0].add_passenger();
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				bus_array[i][j].print_bus();
+				System.out.print("\n");
+			}
 		}
     }
 }
